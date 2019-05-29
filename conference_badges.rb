b61ca5243af55a9b2a7056a1664eef68daa82fd0
Maasa Kono <konomaasa@gmail.com>
2019-05-29 18:49:36 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,10 +22,10 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).map do |person|
+  batch_badge_creator(attendees).each do |person|
     puts person
   end
-  assign_rooms(attendees).map do |person|
+  assign_rooms(attendees).each do |person|
     puts person
   end
 end
